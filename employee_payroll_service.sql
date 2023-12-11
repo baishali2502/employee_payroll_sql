cf50e727dcc8b6faa273cd370745286006f1eaae
baishali2502 <baishali2502@gmail.com>
2023-12-11 05:56:08 +0530

    Added UC2: Create table employee_payroll

--- employee_payroll_service.sql
+++ employee_payroll_service.sql
@@ -1,3 +1,9 @@
 CREATE DATABASE `payroll_service`;
 SHOW DATABASES;
 USE `payroll_service`;
+CREATE TABLE employee_payroll (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    NAME VARCHAR(255),
+    salary DOUBLE,
+    start_date DATE
+);
