0e10e80e31f380fad04771a3ca42d198c938e7d5
baishali2502 <baishali2502@gmail.com>
2023-12-11 06:06:42 +0530

    Added UC5:Retrieve particular employee data

--- employee_payroll_service.sql
+++ employee_payroll_service.sql
@@ -13,5 +13,11 @@ INSERT INTO employee_payroll (NAME, salary, start_date) VALUES
 ('John Doe', 50000.00, '2023-01-01'),
 ('Jane Smith', 60000.00, '2023-02-15'),
 ('Bob Johnson', 70000.00, '2023-03-30');
+INSERT INTO employee_payroll (NAME, salary, start_date) VALUES
+('Bill', 55000.00, '2023-04-10');
 
 SELECT * FROM employee_payroll;
+
+SELECT salary FROM employee_payroll WHERE NAME = 'Bill';
+SELECT * FROM employee_payroll WHERE start_date BETWEEN CAST('2018-01-01' AS DATE) AND DATE(NOW());
+
