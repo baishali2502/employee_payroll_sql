31b2058818559d3a31a66ba13b496421533a3f6d
baishali2502 <baishali2502@gmail.com>
2023-12-11 05:59:09 +0530

    Added UC3:Insert into table values

--- employee_payroll_service.sql
+++ employee_payroll_service.sql
@@ -7,3 +7,7 @@ CREATE TABLE employee_payroll (
     salary DOUBLE,
     start_date DATE
 );
+INSERT INTO employee_payroll (NAME, salary, start_date) VALUES
+('John Doe', 50000.00, '2023-01-01'),
+('Jane Smith', 60000.00, '2023-02-15'),
+('Bob Johnson', 70000.00, '2023-03-30');
